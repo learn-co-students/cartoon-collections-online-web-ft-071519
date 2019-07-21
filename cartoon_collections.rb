22a10fce01edd77a2a18cb12b5369702b667f047
Daniel Siekawitch <daniel.siekawitch@gmail.com>
2019-07-21 05:27:12 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,23 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+# roll_call_dwarves method should accept an array of dwarf names print out each name in number order.
+def roll_call_dwarves(array)
+   array.each_with_index { |word, index| puts "#{index+1} #{word}" }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+# summon_captain_planet accept an array argument of planeteer calls.
+# then capitalize each element and add an exclamation point at the end.
+def summon_captain_planet(array)
+   array.collect { |word| word.capitalize + "!" }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+# long_planeteer_calls method should accept an array of calls. 
+# It should tell us if any of the calls are longer than four characters.
+def long_planeteer_calls(array)
+   array.any? { |word| word.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+# find_the_cheese method should accept an array of strings. 
+# then looks through these strings to find and return the first string that is a type of cheese.
+def find_the_cheese(array)
+   cheese_types = ["cheddar", "gouda", "camembert"]
+   array.find { |cheese| cheese_types.include?(cheese) }
 end
